dd6e838644d7a48a6dc60355730a8f626cdbcf21
Tim LaTorre <timlatorre@gmail.com>
2012-11-26 15:48:08 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Barterville::Application.config.secret_token = 'b3ad4b5fc366297bfb6e2f2d8f318e6b096ca5de1616f95cbc15bb829d11a074292b5c50a15d25f4c716d7327255494e0305b2e4e99a01d23b13946f0999441a'
