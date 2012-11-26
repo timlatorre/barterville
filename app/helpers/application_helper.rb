89311cb4888f94bf2713e1eb175f8d09fc64047b
whitperson <whitperson@gmail.com>
2012-11-26 17:55:41 -0500

    controllers, views, nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def display_links
+    separator = " | "
+    if @auth
+      link_to('profile', profile_path) +
+      link_to('logout', logout_path)
+    else
+      link_to('register', register_path) +
+      link_to('login', login_path)
+    end 
+  end
 end
